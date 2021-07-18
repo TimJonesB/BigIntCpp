5457b3fbb024fc70197e03ebb3d59ec60b882023
TimJonesB <46300779+TimJonesB@users.noreply.github.com>
2021-07-18 18:30:03 -0500

    Header-Only Conversion (#1)
    * start of refactor to make header only
    
    * rm cpp files moving to header only
    
    * placeholder test file
    
    * refactor tests into sep file
    
    * add cmake file for testing
    
    * rename file
    
    * cleanup

--- bigint.h
+++ bigint.h
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include <iostream>
+#include <cmath>
 
 #include "ubigint.h"
 
@@ -53,20 +54,328 @@ public:
     BigInt& randomize(const size_t &length);
     BigInt& power(const BigInt &rhs);
     BigInt& shift10(int m=1);
-    BigInt& abs() {neg = false;}
+    BigInt& abs();
     size_t get_length() {return magnitude.get_length();}
-    uint karatsuba_thres = 64;
+    BigInt& set_karatsuba_thres(size_t thres); 
+    size_t get_karatsuba_thres() {return this->karatsuba_thres;}
 private:
     UBigInt magnitude;
     bool neg = false;
+    size_t karatsuba_thres = 48;
     BigInt karatsuba(BigInt lhs, BigInt rhs);
 };
 
-bool test_arithmatic();
-void test_util();
-void find_karatsuba_thres(std::pair<int, int> k_test_limits={16,128},
-                          int k_test_increment=8,
-                          int n_per_increment=1,
-                          size_t size_number=1000);
+
+BigInt::BigInt(std::string s) {
+    neg = false;
+    int i = 0;
+    while (s[i] == ' ') {
+        i++;
+    }
+    if (s[i] == '-' || s[i] == '+') {
+        if (s[i] == '-') {
+            neg = true;
+        }
+        i++;
+    }
+    magnitude = s.substr(i, s.size());
+    if (magnitude == 0) {
+        neg = false;
+    }
+}
+
+
+std::ostream& operator<<(std::ostream &out, const BigInt &rhs) {
+    if (rhs.neg) {
+        std::cout << '-';
+    }
+    std::cout << rhs.magnitude; 
+    return out;
+}
+
+
+BigInt& BigInt::operator+=(const BigInt &rhs) {
+    if (neg && !rhs.neg) {
+        if (magnitude == rhs.magnitude) {
+            magnitude = {0};
+            neg = false;
+        }
+        else if (magnitude < rhs.magnitude) {
+            neg = false;
+            magnitude = rhs.magnitude - magnitude;
+        }
+        else if (magnitude > rhs.magnitude) {
+            neg = true;
+            magnitude -= rhs.magnitude;
+        }
+    }
+
+    else if (!neg && rhs.neg) {
+        if (magnitude == rhs.magnitude) {
+            magnitude = {0};
+            neg = false;
+        }
+        else if (magnitude < rhs.magnitude) {
+            neg = true;
+            magnitude = rhs.magnitude - magnitude;
+        }
+        else if (magnitude > rhs.magnitude) {
+            neg = false;
+            magnitude -= rhs.magnitude;
+        }
+    }
+
+    else if(!neg && !rhs.neg) {
+        neg = false;
+        magnitude += rhs.magnitude;
+    }
+
+    else {
+        neg = true;
+        magnitude += rhs.magnitude;
+    }
+
+    return *this;
+}
+
+
+BigInt& BigInt::operator-=(const BigInt &rhs) {
+    if (neg && !rhs.neg) {
+        neg = true;
+        magnitude += rhs.magnitude;
+    }
+    else if (!neg && rhs.neg) {
+        neg = false;
+        magnitude += rhs.magnitude;
+    }
+    else if(!neg && !rhs.neg) {
+        if (magnitude == rhs.magnitude) {
+            magnitude = {0};
+            neg = false;
+        }
+        else if (magnitude < rhs.magnitude) {
+            neg = true;
+            magnitude = rhs.magnitude - magnitude;
+        }
+        else if (magnitude > rhs.magnitude) {
+            neg = false;
+            magnitude -= rhs.magnitude;
+        }
+    }
+    else {
+        if (magnitude == rhs.magnitude) {
+            magnitude = {0};
+            neg = false;
+        }
+        else if (magnitude < rhs.magnitude) {
+            neg = false;
+            magnitude = rhs.magnitude - magnitude;
+        }
+        else if (magnitude > rhs.magnitude) {
+            neg = true;
+            magnitude -= rhs.magnitude;
+        }
+    }
+    return *this;
+}
+
+
+BigInt& BigInt::operator*=(const BigInt &rhs) {
+    bool negative = (neg != rhs.neg); 
+    *this = karatsuba(*this, rhs);
+    neg = negative;
+    return *this;
+}
+
+
+BigInt BigInt:: operator/=(const BigInt &rhs) {
+    if (rhs.magnitude > magnitude) {
+        magnitude = 0;
+        neg = false;
+    }
+    else {
+        neg = (neg != rhs.neg);
+        magnitude /= rhs.magnitude;
+    }
+    return *this;
+}
+
+
+BigInt& BigInt::operator++() {
+    *this += 1;
+    return *this;
+}
+
+
+BigInt& BigInt::operator--() {
+    *this -= 1;
+    return *this;
+}
+
+
+BigInt BigInt::operator++(int) {
+    BigInt pre(*this);
+    *this += 1;
+    return pre;
+}
+
+
+BigInt BigInt::operator--(int) {
+    BigInt pre(*this);
+    *this -= 1;
+    return pre;
+}
+
+
+BigInt operator+(const BigInt &lhs, const BigInt &rhs) {
+    return BigInt(lhs) += rhs;
+}
+
+
+BigInt operator-(const BigInt &lhs, const BigInt &rhs) {
+    return BigInt(lhs) -= rhs;
+} 
+
+
+BigInt operator*(const BigInt &lhs, const BigInt &rhs) {
+    return BigInt(lhs) *= rhs;
+}
+
+
+BigInt operator/(const BigInt &lhs, const BigInt &rhs) {
+    return BigInt(lhs) /= rhs;
+}
+
+
+bool operator>(const BigInt &lhs, const BigInt &rhs) {
+    if (lhs.neg && !rhs.neg) {
+        return false;
+    }
+    else if (!lhs.neg && rhs.neg) {
+        return true;
+    }
+    else if (lhs.neg && rhs.neg) {
+        return (lhs.magnitude < rhs.magnitude);
+    }
+    else {
+        return (lhs.magnitude > rhs.magnitude);
+    }
+}
+
+
+bool operator<(const BigInt &lhs, const BigInt &rhs) {
+    if (lhs.neg && !rhs.neg) {
+        return true;
+    }
+    if (!lhs.neg && rhs.neg) {
+        return false;
+    }
+    if (lhs.neg && rhs.neg) {
+        return (lhs.magnitude > rhs.magnitude);
+    }
+    else {
+        return (lhs.magnitude < rhs.magnitude);
+    }
+}
+
+
+bool operator==(const BigInt &lhs, const BigInt &rhs) {
+    if (lhs.neg != rhs.neg) {
+        return false;
+    }
+    return (lhs.magnitude == rhs.magnitude);
+}
+
+
+bool operator!=(const BigInt &lhs, const BigInt &rhs) {
+    return !(lhs == rhs);
+}
+
+
+bool operator>=(const BigInt &lhs, const BigInt &rhs) {
+    return (lhs == rhs || lhs > rhs);
+}
+
+
+bool operator<=(const BigInt &lhs, const BigInt &rhs) {
+    return (lhs == rhs || lhs < rhs);
+}
+
+
+BigInt& BigInt::set_karatsuba_thres(size_t thres) {
+    this->karatsuba_thres = thres;
+    return *this;
+}
+
+
+BigInt BigInt::karatsuba(BigInt lhs, BigInt rhs) {
+    if (lhs.get_length() < karatsuba_thres || rhs.get_length() < karatsuba_thres) {
+        return lhs.magnitude *= rhs.magnitude;
+    }
+    int m = std::max(lhs.get_length(), rhs.get_length());
+    int m2 = std::ceil(m / 2);
+
+    BigInt high1, low1, high2, low2;
+    if (lhs.magnitude.get_length() < m2) {
+        high1 = 0;
+        low1 = lhs;
+    }
+    else {
+        high1 = lhs.magnitude.get_slice(0, lhs.magnitude.get_length() - m2);
+        low1 = lhs.magnitude.get_slice(lhs.magnitude.get_length() - m2, lhs.magnitude.get_length());
+    }
+    if (rhs.magnitude.get_length() < m2) {
+        high2 = 0;
+        low2 = rhs;
+    }
+    else {
+        high2 = rhs.magnitude.get_slice(0, rhs.magnitude.get_length() - m2);
+        low2 = rhs.magnitude.get_slice(rhs.magnitude.get_length() - m2, rhs.magnitude.get_length());
+    }
+
+    BigInt z0 = karatsuba(low1, low2);
+    BigInt z1 = karatsuba((low1 + high1), (low2 + high2));
+    BigInt z2 = karatsuba(high1, high2);
+
+    return ((z2.shift10(m2*2)) + ((z1-z2-z0).shift10(m2)) + z0);
+}
+
+
+BigInt& BigInt::randomize(const size_t &length) {
+    neg = std::rand() % 2;
+    *this = BigInt(UBigInt().randomize(length), neg);
+    return *this;
+}
+
+BigInt& BigInt::power(const BigInt &rhs) {
+    if (rhs == 0) {
+        *this = {1};
+    }
+    else if (rhs < 0) {
+        *this = {0}; 
+    }
+    else {
+        const BigInt base = *this;
+        UBigInt count {0};
+        while (count < rhs-1) {
+            *this *= base;
+            count++;
+        }
+    }
+    return *this;
+}
+
+
+BigInt& BigInt::abs() {
+    neg = false;
+    return *this;
+}
+
+
+BigInt& BigInt::shift10(int m) {
+    magnitude.shift10(m);
+    return *this;
+}
+
 
 #endif
---
+++ tests.cpp
@@ -0,0 +1,149 @@
+#include <cassert>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+#include "bigint.h"
+
+
+void find_karatsuba_thres(std::pair<int, int> k_test_limits={16,128},
+                          int k_test_increment=16,
+                          int n_per_increment=5,
+                          size_t size_number=1000) {
+    /*
+    Calculates optimal threshold number of digits to do Karatsuba multiplication on your machine.
+    */
+    BigInt a = BigInt().randomize(size_number);
+    BigInt b = BigInt().randomize(size_number);
+    std::vector<std::pair<int, double>> kthres_vs_time;
+    std::cout << "Running Karatsuba Multiplication size threshold calibration..." << std::endl;
+    std::cout << "Current Karatsuba Multiplication default threshold : " << BigInt().get_karatsuba_thres() << std::endl;
+    for (int kthres = k_test_limits.first; kthres <= k_test_limits.second; kthres += k_test_increment) {
+        std::cout << "Testing threshold = " << kthres << std::endl;
+        a.set_karatsuba_thres(kthres);
+        b.set_karatsuba_thres(kthres);
+        std::vector<double> itimes;
+        for (int i = 0; i < n_per_increment; i++){
+            std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+            auto sol = a*b;
+            std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+            std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            itimes.push_back(time_span.count());
+        }
+        double sum = std::accumulate(std::begin(itimes), std::end(itimes), 0.0);
+        double mean =  sum / itimes.size();
+        kthres_vs_time.push_back({kthres, mean});
+    }
+    auto optim_digits = std::min_element(kthres_vs_time.begin(), kthres_vs_time.end(),
+                                  [](const auto &lhs, const auto &rhs) {return lhs.second < rhs.second;});
+
+    std::cout << "Optimal karatsuba digits threshold = " << optim_digits->first << std::endl;
+    if (optim_digits->first != BigInt().get_karatsuba_thres()) {
+        std::cout << "Warning: optimal karatsuba multiplication threshold different than current default threshold. It is recommended to change to optimal threshold." << std::endl;
+    }
+    else {
+        std::cout << "Default karatsuba multiplication threshold is optimal" << std::endl;
+    }
+}
+
+
+bool test_arithmatic() {
+    BigInt ta("123");
+    BigInt tb("-123");
+    BigInt tc("48");
+    BigInt td("-48");
+    BigInt te("-309334902454");
+    BigInt tf("2342346");
+    BigInt tg("4928");
+    BigInt th("7423958");
+    BigInt ti("4009281734901283749012384721908472198472109837409123809847985685609847982356978364038976501389653019856398739218732109876021945821094582109487632194576231946032194750329147829823195632985");
+    BigInt tj("72046120374610239849186598742395873129058732904587320958620319483208989764043890389275");
+    BigInt tz("0");
+
+    assert((ta+tz) == ta);
+    assert((ta+ta) == BigInt("246"));
+    assert((ta+tb) == BigInt("0"));
+    assert((ta+tc) == BigInt("171"));
+    assert((ta+td) == BigInt("75"));
+    assert((ta-ta) == BigInt("0"));
+    assert((ta-tb) == BigInt("246"));
+    assert((ta-tc) == BigInt("75"));
+    assert((ta-td) == BigInt("171"));
+    assert((tb+tz) == tb);
+    assert((tb+ta) == BigInt("0"));
+    assert((tb+tb) == BigInt("-246"));
+    assert((tb+tc) == BigInt("-75"));
+    assert((tb+td) == BigInt("-171"));
+    assert((tb-ta) == BigInt("-246"));
+    assert((tb-tb) == BigInt("0"));
+    assert((tb-tc) == BigInt("-171"));
+    assert((tb-td) == BigInt("-75"));
+    assert((tc+ta) == BigInt("171"));
+    assert((tc+tb) == BigInt("-75"));
+    assert((tc+tc) == BigInt("96"));
+    assert((tc+td) == BigInt("0"));
+    assert((tc-ta) == BigInt("-75"));
+    assert((tc-tb) == BigInt("171"));
+    assert((tc-tc) == BigInt("0"));
+    assert((tc-td) == BigInt("96"));
+    assert((td+ta) == BigInt("75"));
+    assert((td+tb) == BigInt("-171"));
+    assert((td+tc) == BigInt("0"));
+    assert((td+td) == BigInt("-96"));
+    assert((ti+tj) == BigInt("4009281734901283749012384721908472198472109837409123809847985685609847982356978364038976501389653019928444859593342349725208544563490455238546365099163552904652514233538137593867086022260"));
+    assert((td-ta) == BigInt("-171"));
+    assert((td-tb) == BigInt("75"));
+    assert((td-tc) == BigInt("-96"));
+    assert((td-td) == BigInt("0"));
+    assert((ti-tj) == BigInt("4009281734901283749012384721908472198472109837409123809847985685609847982356978364038976501389653019784352618844121870026835347078698708980428899289988910987411875267120158065779305243710"));
+
+    assert((ta*tz) == BigInt("0"));
+    assert((ta*tc) == BigInt("5904"));
+    assert((ta*td) == BigInt("-5904"));
+    assert((ta*td) == BigInt("-5904"));
+    assert((tb*td) == BigInt("5904"));
+    assert((te*tf) == BigInt("-724569371423517084"));
+    assert((tg*th) == BigInt("36585265024"));
+    assert((ti*tj) == BigInt("288853194488424069469729017645963098520805528490909790518888657437800905595420860891087371056334964541297711555645041024743502735931106968133713766969809045713583157817868518916076282763336317917556070288954708137868534155993701010900034642641532496448327276296191680235875"));
+
+    assert((ta/tc) == BigInt("2"));
+    assert((ta/td) == BigInt("-2"));
+    assert((tc/ta) == BigInt("0"));
+    assert((tc/tb) == BigInt("0"));
+    assert((tz/ta) == BigInt("0"));
+    assert((tz/tb) == BigInt("0"));
+
+    std::cout << "[PASS] Arithmatic" << std::endl;
+    return true;
+}
+
+
+void test_util() {
+    BigInt ta{"123456789"};
+    assert(ta.shift10() == BigInt(1234567890));
+    assert(ta.shift10(-1) == BigInt(123456789));
+    assert(ta.shift10(3) == BigInt("123456789000"));
+    assert(ta.shift10(-4) == BigInt(12345678));
+
+    BigInt tb{"1234567"};
+    assert(tb.get_length() == 7);
+    assert((tb-tb).get_length() == 1);
+
+    BigInt tc{"452349"};
+    assert((tc++) == BigInt("452349"));
+    assert((tc) == BigInt("452350"));
+
+    BigInt td{"452349"};
+    assert((++td) == BigInt("452350"));
+
+    std::cout << "[PASS] Utilities" << std::endl;
+}
+
+
+int main() {
+    test_arithmatic();
+    test_util();
+    find_karatsuba_thres();
+    return 0;
+}
