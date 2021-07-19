4226b60b0b6c5763ef8af6ff61c06495d4f3025e
TimJonesB <46300779+TimJonesB@users.noreply.github.com>
2021-07-19 00:15:48 -0500

    fix util test (#3)

--- test/utility-test.cpp
+++ test/utility-test.cpp
@@ -3,20 +3,19 @@
 
 TEST (utility, utility_tests){
     BigInt ta{"123456789"};
-    assert(ta.shift10() == BigInt(1234567890));
-    assert(ta.shift10(-1) == BigInt(123456789));
-    assert(ta.shift10(3) == BigInt("123456789000"));
-    assert(ta.shift10(-4) == BigInt(12345678));
+    EXPECT_EQ(ta.shift10(), BigInt(1234567890));
+    EXPECT_EQ(ta.shift10(-1), BigInt(123456789));
+    EXPECT_EQ(ta.shift10(3), BigInt("123456789000"));
+    EXPECT_EQ(ta.shift10(-4), BigInt(12345678));
 
     BigInt tb{"1234567"};
-    assert(tb.get_length() == 7);
-    assert((tb-tb).get_length() == 1);
+    EXPECT_EQ(tb.get_length(), 7);
+    EXPECT_EQ((tb-tb).get_length(), 1);
 
     BigInt tc{"452349"};
-    assert((tc++) == BigInt("452349"));
-    assert((tc) == BigInt("452350"));
+    EXPECT_EQ(tc++, BigInt("452349"));
+    EXPECT_EQ(tc, BigInt("452350"));
 
     BigInt td{"452349"};
-    assert((++td) == BigInt("452350"));
-
+    EXPECT_EQ((++td), BigInt("452350"));
 }
