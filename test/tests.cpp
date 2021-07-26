2b92c45c3d70db588f3fc4d966c0dd2c6ce39933
= <tjones189325@gmail.com>
2021-07-25 20:07:46 -0500

    finish docs

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+/**
+ * @brief  Entry point for module which runs all available gtests on BigIntCpp header only library.
+ */
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
