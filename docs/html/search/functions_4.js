758507a2edab866af6b0a7d7d950aea0650e5dc8
TimJonesB <46300779+TimJonesB@users.noreply.github.com>
2021-07-30 11:30:15 -0500

    Benchmark (#5)
    * update docs
    
    * add benchmark tests
    
    * fix subtraction bug and update style
    
    * update readme

--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['randomize_36',['randomize',['../classBigInt.html#ac7aa2fb7fb429858de9bf178b13bb89f',1,'BigInt::randomize()'],['../classUBigInt.html#a1f101aad3d139b317adc853a3fbf6ae0',1,'UBigInt::randomize()']]]
+  ['power_36',['power',['../classBigInt.html#a020df2016d61d371ade06453fb6f8021',1,'BigInt::power()'],['../classUBigInt.html#ac52f6ec69136dc0dec6b174ae137b894',1,'UBigInt::power()']]]
 ];
